62de319b017aceb7b2ce7fc75f63869b561e8600
Sean Harnett <srharnett@gmail.com>
2011-09-06 02:12:38 -0400

    added google graph

--- mobileAC/models.py
+++ mobileAC/models.py
@@ -28,6 +28,31 @@ class Thermometer(models.Model):
     name = models.CharField(max_length=30)
     def __unicode__(self):
         return self.name
+    def googleGraph(self):
+        n = 4 # number of readings to graph
+        readings = self.thermoreading_set.all()[:n]
+        temps = ','.join([str(reading.temperature) for reading in readings])
+        hums = ','.join([str(reading.humidity) for reading in readings])
+        src = '''
+        "http://chart.apis.google.com/chart
+        ?chxl=0:|-3hr|-2hr|-1hr|Now|1:|40%%|50%%|60%%|70%%|2:|50&deg;|70&deg;|90&deg;
+        &chxp=0,0,34,68,100
+        &chxr=1,50,90|2,40,70
+        &chxs=0,00AA00,13.333,0.5,l,676767|2,676767,10.833,0,l,676767
+        &chxt=x,r,y
+        &chs=250x168
+        &cht=lc
+        &chco=FF0000,0000FF
+        &chds=50,90,40,70
+        &chd=t:%s|%s
+        &chdl=Temperature+(&deg;F)|%%25+Humidity
+        &chdlp=b
+        &chg=34,0,0,0
+        &chma=|4
+        &chtt=Current+Conditions"''' % (temps, hums)
+        src = ''.join(src.split())
+        myUrl = '''<img src=%s width="250" height="168" alt="Current Conditions" />''' % src
+        return myUrl
 
 class ThermoReading(models.Model):
     thermometer = models.ForeignKey(Thermometer)
@@ -36,3 +61,5 @@ class ThermoReading(models.Model):
     humidity = models.DecimalField(max_digits=5, decimal_places=1)
     def __unicode__(self):
         return "temp: %5.1f hum: %5.1f" % (self.temperature, self.humidity)
+    class Meta:
+        ordering = ['-time']
