23d9cdcadd71503f846fcb91029ce840d70bb9e8
Sean Harnett <srharnett@gmail.com>
2013-06-10 21:02:56 -0400

    refactor

--- temp_sensor.py
+++ temp_sensor.py
@@ -1,23 +1,24 @@
 import serial
 from time import sleep
+from os import environ
 
 class TempSensor:
     def __init__(self):
-        device = '/dev/tty.usbmodemfd121'
+        device = '/dev/' + environ['DEVICE']
         self.connection = serial.Serial(device, 9600)
         sleep(2)
     def read(self):
-        try:    
-            self.connection.write(' ')    
+        return self.command('t')
+    def command(self, c):
+        self.connection.write(c)
+        try:
             return self.connection.readline()
         except:
             return 'failed'
-    def command(self, c):
-        self.connection.write(c)    
 
 if __name__ == '__main__':
     x = raw_input('q to quit: ')
     t = TempSensor()
     while x != 'q':
-        t.command(x)
+        print t.command(x)
         x = raw_input('q to quit: ')
